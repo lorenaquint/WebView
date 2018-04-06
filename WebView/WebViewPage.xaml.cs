20e845d01e647e8465fe8d548fb626c1adde906f
lorenaquint <lorenaquint@gmail.com>
2018-04-06 15:04:33 -0500

    Initial commit

---
+++ WebView/WebViewPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace WebView
+{
+    public partial class WebViewPage : ContentPage
+    {
+        public WebViewPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+        {
+            var canGoBack = webview.CanGoBack;
+            var canGoFormware = webview.CanGoForward;
+            await DisplayAlert("Estado",
+                               "se ha terminado de cargar la pagina",
+                               "Aceptar");
+            if (canGoBack)
+            {
+                webview.GoBack();
+            }
+        }
+
+         void  Handle_Navigating(object sender, Xamarin.Forms.WebNavigatingEventArgs e)
+        {
+            
+           
+        }
+    }
+}
